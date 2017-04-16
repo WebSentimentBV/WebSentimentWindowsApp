4677d719b5b3cfa240f989d83b886685832125c9
TarikHaci <tarikhaci@outlook.com>
2017-04-17 00:09:57 +0200

    Begin aan het project gemaakt, en mappenstructuur verbeterd

---
+++ Applicatie/WebSentiment/WebSentiment/Classes/DatabaseManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net;
+namespace E_Divison.Classes
+{
+    class DatabaseManager
+    {
+        private SQLite.Net.SQLiteConnection con;
+        private string databaseName;
+        private string path;
+        public DatabaseManager()
+        {
+            Init();
+        }
+        private void Init()
+        {
+            databaseName = "db.sqlite";
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, databaseName);
+            con = new SQLite.Net.SQLiteConnection(new
+            SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+        }
+
+        public SQLiteConnection GetCon()
+        {
+            return con;
+        }
+    }
+}
---
+++ Applicatie/WebSentiment/WebSentiment/Classes/ImageManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace E_Divison.Classes
+{
+    public class ImageManager
+    {
+        public async void SetImage(Image imageControl, byte[] imageArray)
+        {
+            if (imageArray != null)
+            {
+                BitmapImage imageMap = new BitmapImage();
+                imageMap.SetSource(await ConvertTo(imageArray));
+                imageControl.Source = imageMap;
+            }
+        }
+
+        private async Task<InMemoryRandomAccessStream> ConvertTo(byte[] arr)
+        {
+            InMemoryRandomAccessStream randomAccessStream = new InMemoryRandomAccessStream();
+            await randomAccessStream.WriteAsync(arr.AsBuffer());
+            randomAccessStream.Seek(0);
+            return randomAccessStream;
+        }
+    }
+}
