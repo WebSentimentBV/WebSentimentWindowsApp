68b90d2d565524624c59a324dc06554b362acafd
TarikHaci <tarikhaci@outlook.com>
2017-06-08 12:22:13 +0200

    Aantaldingen

--- Applicatie/WebSentiment/WebSentiment/MainPage.xaml.cs
+++ Applicatie/WebSentiment/WebSentiment/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,6 +25,7 @@ namespace WebSentiment
         public MainPage()
         {
             this.InitializeComponent();
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(500, 500));
             LoadPage();
         }
 
